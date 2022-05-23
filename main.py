e2c44c6844f86101ead5bb454f74249e71f84096
karl jahnel <karl-augustin.jahnel@stud.htwk-leipzig.de>
2022-05-23 07:42:15 +0200

    first try of generating first generation

--- main.py
+++ main.py
@@ -2,8 +2,25 @@ import cv2
 import numpy as np
 
 #basis edge set
-region1 = np.array([[255, 255, 0, 0],[255, 255, 0, 0],[255, 255, 0, 0],[255, 255, 0, 0]])
-region2 = np.array([[0, 0, 255, 255],[0, 0, 255, 255],[0, 0, 255, 255],[0, 0, 255, 255]])
+#left right
+region1_1 = np.array([[255, 255, 0, 0],[255, 255, 0, 0],[255, 255, 0, 0],[255, 255, 0, 0]])
+region2_1 = np.array([[0, 0, 255, 255],[0, 0, 255, 255],[0, 0, 255, 255],[0, 0, 255, 255]])
+
+
+#top down
+region1_2= np.array([[255, 255, 255, 255],[255, 255, 255, 255],[0, 0, 0, 0],[0, 0, 0, 0]])
+region2_2 = np.array([[0, 0, 0, 0],[0, 0, 0, 0],[255, 255, 255, 255],[255, 255, 255, 255]])
+
+#diagonal
+region1_3 = np.array([[255, 255, 255, 255],[255, 255, 0, 0],[255, 0, 0, 0],[0, 0, 0, 0]])
+region2_3 = np.array([[0, 0, 0, 0],[0, 0, 255, 255],[0, 255, 255, 255],[255, 255, 255, 255]])
+
+#diagonal reverse
+region1_4 = np.array([[255, 255, 255, 255],[0, 255, 255, 255],[0, 0, 255, 255],[0, 0, 0, 0]])
+region2_4 = np.array([[0, 0, 0, 0],[0, 0, 0, 255],[255, 255, 0, 0],[255, 255, 255, 255]])
+
+
+regions=[(region1_1,region1_2),(region1_2,region2_2),(region1_3,region2_3),(region1_4,region2_4)]
 # w means weight for the Cost factors Curvature, Dissimilarity, number of edge points, fragmentation, thickness
 def curvature(edgeConfiguration):
     pass
@@ -33,20 +50,28 @@ def generatefirstgeneration(gray):
     dissimilarity_start_gen = np.zeros(gray.shape, dtype=np.uint8)
     for x in range(gray.shape[0]):
         for y in range(gray.shape[1]):
-            fittededgestructure = edgeStructure(gray[x-2:x+2,y-2:y+2])
-            dissimilarity_start_gen[x,y] = dissimilaritymeasure(fittededgestructure)
+            fitt=0
+            for pattern in regions:
+                fittededgestructure = edgeStructure(gray[x - 2:x + 2, y - 2:y + 2],pattern[0],pattern[1])
+                newfitt=dissimilaritymeasure(fittededgestructure)
+
+                if newfitt>fitt:
+                    fitt=newfitt
+            dissimilarity_start_gen[x, y]= fitt
     return dissimilarity_start_gen
 
 # S is a edgeStructure
 def dissimilaritymeasure(S):
-    print(abs(S.R2.mean() - S.R2.mean()))
-    return abs(S.R2.mean() - S.R2.mean())
+    return abs(S.R2.mean() - S.R1.mean())
 
 
 class edgeStructure:
+    l0=[]
+    l1=[]
+    l2=[]
     R1= np.zeros([4,4],dtype=np.uint8)
     R2= np.zeros([4,4],dtype=np.uint8)
-    def __init__(self, edgeneighbourhood):
+    def __init__(self, edgeneighbourhood,region1,region2):
         if edgeneighbourhood.shape[0]==4 and edgeneighbourhood.shape[1]==4:
             self.R1 = edgeneighbourhood.copy()
             self.R1[region1==0] =0
